d7508d84192c38f9ad4004924d3115c8d1cfe526
katiershook <53106073+katiershook@users.noreply.github.com>
2019-12-11 19:33:13 -0800

    first commit
    one card showing

--- public_api_request-v1/app.js
+++ public_api_request-v1/app.js
@@ -1,3 +1,6 @@
+
+const urlApi =  'https://randomuser.me/api/?results=12&nat=us' ;
+
 const html = //`search.className = 'search-container'
 `<form action="#" method="get">
 <input type="search" id="search-input" class="search-input" placeholder="Search...">
@@ -5,10 +8,42 @@ const html = //`search.className = 'search-container'
 </form>`
 //creates a div
 var searchBar = document.createElement('div');
-//assigns an id
+//assigns a class
 searchBar.class = "search-container";
 // assigns the var html to the search bar divs inner HTML
 searchBar.innerHTML = html;
 // appens that bad boy to the page
 document.body.appendChild(searchBar);
-console.log('hi');
+
+
+const cardDiv = `
+<div class="card">
+<div class="card-img-container">
+    <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+</div>
+<div class="card-info-container">
+    <h3 id="name" class="card-name cap">first last</h3>
+    <p class="card-text">email</p>
+    <p class="card-text cap">city, state</p>
+</div>
+</div>`
+
+
+const gallery = document.createElement('div');
+gallery.class ="card";
+gallery.innerHTML = cardDiv;
+document.body.appendChild(gallery);
+
+//--------------------------------------
+// FETCH FUNCTIONS
+ fetch('https://randomuser.me/api/?results=12&nat=us')
+     .then(response => response.json())
+     .then(data => console.log(data));
+     //.then(data => generateEmployee(date.message))
+  
+
+// // //   // ---------------------------
+//   //HELPER FUNCTIONS
+//  // 
+//  function generateEmployee(data){
+// const imageHtml = `<img src =`${data}` alt >
