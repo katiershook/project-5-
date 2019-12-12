19b90dc8c5401083a66da687b3b44446c2f5fd15
katiershook <53106073+katiershook@users.noreply.github.com>
2019-12-11 20:51:04 -0800

    show all 12 cards
    all 12 cards appended to the page

--- public_api_request-v1/app.js
+++ public_api_request-v1/app.js
@@ -1,7 +1,7 @@
 
 const urlApi =  'https://randomuser.me/api/?results=12&nat=us' ;
 
-const html = //`search.className = 'search-container'
+const searchBarHtml= //`search.className = 'search-container'
 `<form action="#" method="get">
 <input type="search" id="search-input" class="search-input" placeholder="Search...">
 <input type="submit" value="&#x1F50D;" id="search-submit" class="search-submit">
@@ -11,39 +11,66 @@ var searchBar = document.createElement('div');
 //assigns a class
 searchBar.class = "search-container";
 // assigns the var html to the search bar divs inner HTML
-searchBar.innerHTML = html;
+searchBar.innerHTML = searchBarHtml;
 // appens that bad boy to the page
 document.body.appendChild(searchBar);
 
 
-const cardDiv = `
-<div class="card">
-<div class="card-img-container">
-    <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
-</div>
-<div class="card-info-container">
-    <h3 id="name" class="card-name cap">first last</h3>
-    <p class="card-text">email</p>
-    <p class="card-text cap">city, state</p>
-</div>
-</div>`
+// const cardDiv = `
+// <div class="card">
+// <div class="card-img-container">
+//     <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+// </div>
+// <div class="card-info-container">
+//     <h3 id="name" class="card-name cap">
+//     name </h3>
+//     <p class="card-text">email</p>
+//     <p class="card-text cap">city, state</p>
+// </div>
+// </div>`
 
 
 const gallery = document.createElement('div');
 gallery.class ="card";
-gallery.innerHTML = cardDiv;
+//gallery.innerHTML = cardDiv;
 document.body.appendChild(gallery);
 
 //--------------------------------------
 // FETCH FUNCTIONS
  fetch('https://randomuser.me/api/?results=12&nat=us')
      .then(response => response.json())
-     .then(data => console.log(data));
-     //.then(data => generateEmployee(date.message))
-  
+    // .then(data => console.log(data))
+     .then(data => generateEmployee(data.results))
+   
+
+    
 
 // // //   // ---------------------------
 //   //HELPER FUNCTIONS
 //  // 
-//  function generateEmployee(data){
+
+ function generateEmployee(data){
+
+ const employees = data.map(item =>
+
+     `
+     <div class="card">
+     <div class="card-img-container">
+         <img class="card-img" src="https://placehold.it/90x90" alt="profile picture">
+     </div>
+     <div class="card-info-container">
+         <h3 id="name" class="card-name cap">
+         ${item.name}</h3>
+         <p class="card-text">email</p>
+         <p class="card-text cap">city, state</p>
+     </div>
+     </div>
+ 
+
+     `)
+     
+    
+    gallery.innerHTML = employees 
+ }
+  
 // const imageHtml = `<img src =`${data}` alt >
